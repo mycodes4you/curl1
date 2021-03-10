1dcc9ce371d18fa6cbc4b3d808c5af15574e4b95
Alex Laptop <alejandro@micrositios.com>
2021-03-10 17:56:08 -0600

    Update subida.php

--- subida.php
+++ subida.php
@@ -13,7 +13,7 @@ $fichero_subido = $dir_subida . basename($_FILES['archivo']['name']);
 
 if (move_uploaded_file($_FILES['archivo']['tmp_name'], $fichero_subido)) {
 	chmod($fichero_subido, 0777);
-  echo "El fichero es válido y se subió con éxito.\n";
+  echo "El fichero es válido y se subió con éxito.<br>";
   echo '<img src="https://'.$_SERVER['SERVER_NAME'].'/'.$fichero_subido.'">';
 } else {
   echo "¡Error al subir el archivo!\n";
