316410d3c1ae8973b9d229b437ee752c34549e4a
Alex Laptop <alejandro@micrositios.com>
2021-03-10 16:43:02 -0600

    Update subida.php

--- subida.php
+++ subida.php
@@ -4,11 +4,11 @@ if(!empty($_POST)){
 	//print_r($_POST); 
 }
 if(!empty($_FILES)){ 
-	http_response_code(200);
+	//http_response_code(200);
 	//print_r($_FILES); 
 	$response = 'El archivo se recibio de manera correcta';
 }else{
-	http_response_code(403);
+	//http_response_code(403);
 	$response = 'El archivo no se pudo recibir';
 }
 ?>
