c72466f06bcbcd943373d79f93473fdb42632446
Alex Laptop <alejandro@micrositios.com>
2021-03-10 17:32:04 -0600

    1.7

--- subida.php
+++ subida.php
@@ -1,21 +1,4 @@
 <?php
-/*
-if(!empty($_POST)){ 
-	//print_r($_POST); 
-}
-if(!empty($_FILES)){ 
-	//http_response_code(200);
-	//print_r($_FILES); 
-	//echo 'El archivo se recibio de manera correcta';
-	$respuesta = 'El archivo se recibio de manera correcta';
-	echo $respuesta;
-}else{
-	//http_response_code(403);
-	//echo 'El archivo no se pudo recibir';
-	$respuesta = 'El archivo no se recibio de manera correcta';
-	echo $respuesta;
-}*/
-
 
 $dir_subida = 'subidas/';
 $fichero_subido = $dir_subida . basename($_FILES['archivo']['name']);
@@ -23,16 +6,11 @@ $fichero_subido = $dir_subida . basename($_FILES['archivo']['name']);
 echo '<pre>';
 if (move_uploaded_file($_FILES['archivo']['tmp_name'], $fichero_subido)) {
 	chmod($fichero_subido, 0777);
-	echo $fichero_subido;
-    echo "El fichero es válido y se subió con éxito.\n";
+  echo "El fichero es válido y se subió con éxito.\n";
 } else {
-    echo "¡Posible ataque de subida de ficheros!\n";
+  echo "¡Posible ataque de subida de ficheros!\n";
 }
 
-echo 'Más información de depuración:';
-print_r($_FILES);
-
-print "</pre>";
 
 echo '<img src="'.$fichero_subido.'">';
 
