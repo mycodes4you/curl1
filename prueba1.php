73bbbed9e3623d90a25d611614d6e99c42907761
Alex Laptop <alejandro@micrositios.com>
2021-03-10 17:23:06 -0600

    1.6

--- prueba1.php
+++ prueba1.php
@@ -1,6 +1,6 @@
 <?php
 
-$file_url = "archivos/20201123-16.11.34.xml";
+$file_url = "archivos/DF7344-24f4bf23-c831-4421-835c-3f0b9385119a.xml";
 $eol = "\r\n"; //default line-break for mime type
 $BOUNDARY = md5(time()); //random boundaryid, is a separator for each param on my post curl function
 $BODY=""; //init my curl body
@@ -14,7 +14,7 @@ $data = array(
 $datos = json_encode($data);
 $BODY .= "" . $datos. $eol;//param data in this case is a simple post data and 1 $eol for the end of the data
 $BODY.= '--'.$BOUNDARY. $eol; // start 2nd param,
-$BODY.= 'Content-Disposition: form-data; name="archivo"; filename="20201123-16.11.34.xml"'. $eol ; //first Content data for post file, remember you only put 1 when you are going to add more Contents, and 2 on the last, to close the Content Instance
+$BODY.= 'Content-Disposition: form-data; name="archivo"; filename="DF7344-24f4bf23-c831-4421-835c-3f0b9385119a.xml"'. $eol ; //first Content data for post file, remember you only put 1 when you are going to add more Contents, and 2 on the last, to close the Content Instance
 $BODY.= 'Content-Type: application/octet-stream' . $eol; //Same before row
 $BODY.= 'Content-Transfer-Encoding: base64' . $eol . $eol; // we put the last Content and 2 $eol,
 $BODY.= chunk_split(base64_encode(file_get_contents($file_url))) . $eol; // we write the Base64 File Content and the $eol to finish the data,
