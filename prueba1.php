f0691b944d125724b31dc0186e2f235598f714b7
Alex Laptop <alejandro@micrositios.com>
2021-03-10 16:37:37 -0600

    Update prueba1.php

--- prueba1.php
+++ prueba1.php
@@ -35,7 +35,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $BODY); // set our $BODY
 
 $response = curl_exec($ch); // start curl navigation
 
-if(var_dump(http_response_code()) == ínt(200) ){
+if(var_dump(http_response_code()) == 'int(200)' ){
  echo 'El Archivo se recibio de manera correcta';
 } 
 
