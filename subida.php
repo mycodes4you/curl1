746b9b8a3145f4c63598211d95d298287243f052
Alex Laptop <alejandro@micrositios.com>
2021-03-10 17:08:38 -0600

    Update subida.php

--- subida.php
+++ subida.php
@@ -23,6 +23,7 @@ $fichero_subido = $dir_subida . basename($_FILES['archivo']['name']);
 echo '<pre>';
 if (move_uploaded_file($_FILES['archivo']['tmp_name'], $fichero_subido)) {
 	chmod($dir_subida.$fichero_subido, 0777);
+	echo $dir_subida.$fichero_subido;
     echo "El fichero es válido y se subió con éxito.\n";
 } else {
     echo "¡Posible ataque de subida de ficheros!\n";
