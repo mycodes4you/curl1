767c1df4794dd6e30cfa3af295063fd1d7e00e5d
Alex Laptop <alejandro@micrositios.com>
2021-03-11 16:19:55 -0600

    2

--- prueba1.php
+++ prueba1.php
@@ -2,6 +2,47 @@
 $url_archivo = "imagenes/gato.jpg";
 function postcUrl($url_archivo){
 	//$url_archivo = "archivos/DF7344-24f4bf23-c831-4421-835c-3f0b9385119a.xml";
+	$token = md5(time());
+
+	$eol = "\r\n";
+	$BOUNDARY = md5(time()); 
+	$BODY=""; 
+	$BODY.= '--'.$BOUNDARY. $eol; 
+	$BODY .= 'Content-Disposition: form-data; name="Datos"' . $eol . $eol; 
+	$data = array(
+	  'token' => $token,
+	  'usuario' => '701',
+	  'pass' => 'Rjfge'
+	);
+	$datos = json_encode($data);
+	$BODY .= "" . $datos. $eol;
+	$BODY.= '--'.$BOUNDARY. $eol;
+	/*$BODY.= 'Content-Disposition: form-data; name="archivo"; filename="gato.jpg"'. $eol ; 
+	$BODY.= 'Content-Type: application/octet-stream' . $eol; 
+	$BODY.= 'Content-Transfer-Encoding: base64' . $eol . $eol; 
+
+	$BODY.= file_get_contents($url_archivo) . $eol; */
+	$BODY.= '--'.$BOUNDARY .'--' . $eol. $eol;        
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	  'X_PARAM_TOKEN : 71e2cb8b-42b7-4bf0-b2e8-53fbd2f578f9',
+	  "Content-Type: multipart/form-data; boundary=".$BOUNDARY)
+	  );
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/1.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0'); 
+	curl_setopt($ch, CURLOPT_URL, "https://curl.mycodes4you.com/firmar.php"); 
+	curl_setopt($ch, CURLOPT_COOKIEJAR, $BOUNDARY); 
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1); 
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $BODY); 
+
+	$response = curl_exec($ch); /*
+
+
+
+
+
+
 
 	$eol = "\r\n";
 	$BOUNDARY = md5(time()); 
@@ -38,7 +79,7 @@ function postcUrl($url_archivo){
 	$response = curl_exec($ch); 
 
 	return $response;
-
+*/
 }
 
 echo postcUrl($url_archivo);
