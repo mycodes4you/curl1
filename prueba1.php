314dea62cd0361d7097230e053fa3712a2c5c520
Alex Laptop <alejandro@micrositios.com>
2021-03-11 16:34:20 -0600

    Update prueba1.php

--- prueba1.php
+++ prueba1.php
@@ -1,6 +1,25 @@
 <?php
 $url_archivo = "imagenes/gato.jpg";
 function postcUrl($url_archivo){
+
+	/// --- FIRMAMOS PARA ATENTICAR
+	$url_firma = "https://curl.mycodes4you.com/firma.php";
+	$datos = array(
+		  'user' => 'alex',
+		  'pass' => 'rjf7y',
+		  'date' => date('Y-m-d H:m:s')
+		);
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url_firma);
+	curl_setopt($ch, CURLOPT_POST, TRUE);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $datos);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$firma = curl_exec ($ch);
+	curl_close ($ch);
+
+	return $firma;
+
 	//$url_archivo = "archivos/DF7344-24f4bf23-c831-4421-835c-3f0b9385119a.xml";
 	/*$token = md5(time());
 
@@ -85,31 +104,6 @@ function postcUrl($url_archivo){
 */
 
 
-// abrimos la sesión cURL
-$ch = curl_init();
-
-// definimos la URL a la que hacemos la petición
-curl_setopt($ch, CURLOPT_URL,"https://curl.mycodes4you.com/firma.php");
-// indicamos el tipo de petición: POST
-curl_setopt($ch, CURLOPT_POST, TRUE);
-// definimos cada uno de los parámetros
-$data = array(
-	  'user' => 'alex',
-	  'pass' => 'rjf7y',
-	  'date' => date('Y-m-d H:m:s')
-	);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-// recibimos la respuesta y la guardamos en una variable
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$remote_server_output = curl_exec ($ch);
-
-// cerramos la sesión cURL
-curl_close ($ch);
-
-// hacemos lo que queramos con los datos recibidos
-// por ejemplo, los mostramos
-return $remote_server_output;
 
 }
 
